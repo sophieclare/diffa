61429544919114b2b52adf1705816fe28ef9863b
Ben Hood <0x6e6562@gmail.com>
2012-09-10 13:13:36 +0100

    Arrange for new S3 layout

--- s3_upload.sh
+++ s3_upload.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-BUCKET=s3://diffa-packages
-
-sed s/DIFFA_BUILD_NUMBER/$1/ etc/packages.js.in > packages.js
-
-s3cmd rb --recursive $BUCKET
-
-s3cmd mb $BUCKET
+BUCKET=s3://diffa-builds
 
 s3cmd put --acl-public --guess-mime-type dist/target/*.zip $BUCKET
 s3cmd put --acl-public --guess-mime-type agent/target/*.war $BUCKET
-s3cmd put --acl-public --guess-mime-type packages.js $BUCKET
-
-# Upload the zip package to the archives directory for posterity
-
-s3cmd cp --acl-public --guess-mime-type $BUCKET/diffa-b$1.zip s3://diffa-archives
