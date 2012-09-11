8bd5f4fb7fa758e94223b2e38b8fff7c0fb5ed62
Anthony Harris <anthony@lshift.net>
2012-09-11 13:55:27 +0100

    Clean up imports (minimise diff).

--- agent/src/main/scala/net/lshift/diffa/agent/rest/DomainResource.scala
+++ agent/src/main/scala/net/lshift/diffa/agent/rest/DomainResource.scala
@@ -32,11 +32,10 @@ import net.lshift.diffa.kernel.util.AlertCodes._
 import net.lshift.diffa.kernel.config.system.CachedSystemConfigStore
 import net.lshift.diffa.kernel.limiting.DomainRateLimiterFactory
 import net.lshift.diffa.agent.rest.ResponseUtils._
-import net.lshift.diffa.kernel.differencing.{DomainDifferenceStore, DifferencesManager}
-import net.lshift.diffa.kernel.config._
-import org.springframework.security.access.PermissionEvaluator
-import net.lshift.diffa.kernel.config.User
 import net.lshift.diffa.kernel.frontend.EscalationDef
+import net.lshift.diffa.kernel.differencing.{DomainDifferenceStore, DifferencesManager}
+import net.lshift.diffa.kernel.config.{BreakerHelper, DomainConfigStore, DomainCredentialsManager, User}
+import org.springframework.security.access.PermissionEvaluator
 import net.lshift.diffa.agent.rest.PermissionUtils._
 import net.lshift.diffa.agent.auth.{SpaceTarget, Privileges, SpacePrivilege}
 
